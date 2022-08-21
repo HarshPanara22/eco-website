9eabafe92ffd5715111be16acf3d44f8009a7ee5
tunedcoder <thetunedcoder@gmail.com>
2022-08-21 21:22:48 +0530

    Counter Updated

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 8, 30, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2022, 8, 23, 8)}); // year, month, date, hour
         });
 });		
 
